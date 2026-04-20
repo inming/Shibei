efada648728c88290560e0b7c7d91d3f30c971a7
Ming Yin <inming521@gmail.com>
2026-04-20 22:10:29 +0800

    fix(harmony): expose pdfjsWorker global in worker bundle
    pdfjs's fake-worker path looks up globalThis.pdfjsWorker.WorkerMessageHandler
    after importing the workerSrc. Previous bundle had no --global-name, so
    the IIFE returned but never assigned to a global, producing:
      'Setting up fake worker failed: Cannot read properties of
       undefined (reading setup)'
    
    Rebundled with --global-name=pdfjsWorker so 'var pdfjsWorker = (...)()'
    exports the module namespace (including WorkerMessageHandler) onto the
    main-thread global, which fake-worker then resolves successfully.

--- shibei-harmony/entry/src/main/resources/rawfile/pdfjs-demo/pdf-worker-bundle.js
+++ shibei-harmony/entry/src/main/resources/rawfile/pdfjs-demo/pdf-worker-bundle.js
@@ -1,5 +1,27 @@
-(() => {
+var pdfjsWorker = (() => {
+  var __defProp = Object.defineProperty;
+  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+  var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __hasOwnProp = Object.prototype.hasOwnProperty;
+  var __export = (target, all) => {
+    for (var name in all)
+      __defProp(target, name, { get: all[name], enumerable: true });
+  };
+  var __copyProps = (to, from, except, desc) => {
+    if (from && typeof from === "object" || typeof from === "function") {
+      for (let key of __getOwnPropNames(from))
+        if (!__hasOwnProp.call(to, key) && key !== except)
+          __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+    }
+    return to;
+  };
+  var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
   // node_modules/pdfjs-dist/build/pdf.worker.mjs
+  var pdf_worker_exports = {};
+  __export(pdf_worker_exports, {
+    WorkerMessageHandler: () => WorkerMessageHandler
+  });
   var import_meta = {};
   var __webpack_require__ = {};
   (() => {
@@ -62298,4 +62320,5 @@
   globalThis.pdfjsWorker = {
     WorkerMessageHandler
   };
+  return __toCommonJS(pdf_worker_exports);
 })();
